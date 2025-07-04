5a6ec3eb1af9cfe30b0609de8646cae2718a83ed
Jp <bbbhgggfghhf@gmail.com>
2025-07-04 15:48:56 -0400

    Jogo de par ou impar

--- parimpar.py
+++ parimpar.py
@@ -1,9 +1,20 @@
-#input par ou impar var= escolha
-#input do numero x
-#gerar segundo numero y
-#soma = x + y
+import random
+
 #if soma % 2 = 0 and escolha = par { print("Jogador ganhou")}
 #elif soma % 2 = 1 and escolha = impar { print("Jogador ganhou")}
 #else print("Perdeu playboy")
+escolha = input('Escolha par ou impar: ')
+e2 = int(input('Escolha um numero: '))
+e3 = random.randint(1,20)
+e4 = e2 + e3
+print(e4 % 2)
+if(e4 % 2==0 and escolha == "par"):
+    print("Voce ganhou")
+elif(e4 % 2==1 and escolha =="impar"):
+    print("Voce ganhou")
+else : print("Voce perdeu")
 
-print(99 % 2)
+
+
+
+

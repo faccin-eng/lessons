e1def33540bf6316f9dbf250e369f1e0dda953a1
Jp <bbbhgggfghhf@gmail.com>
2025-07-04 15:04:12 -0400

    Calculadora de bashkara

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import math
 
-x = int(input('Insira o valor de X '))
-y = 9
-z = 10
-a = (x+y+z)/3
-print(f'A media é ', a)
+a = int(input('Insira o valor de X '))
+b = int(input('Insira o valor de y' ))
+c = int(input('Insira o valor de z' ))
+delta = b^2 - (-4*a*c)
+print(delta)
+x1 = -b + math.sqrt(delta) /(2*a)
+x2 = -b - math.sqrt(delta) /(2*a)
+print(x1,x2)
+
+

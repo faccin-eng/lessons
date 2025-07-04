48838ab5b8eb20aafb06f77568b3c63865227555
Faccin-Eng <mjfaccin@hotmail.com>
2025-07-04 15:22:32 -0400

    Instruções do par ou impar

---
+++ parimpar.py
@@ -0,0 +1,9 @@
+#input par ou impar var= escolha
+#input do numero x
+#gerar segundo numero y
+#soma = x + y
+#if soma % 2 = 0 and escolha = par { print("Jogador ganhou")}
+#elif soma % 2 = 1 and escolha = impar { print("Jogador ganhou")}
+#else print("Perdeu playboy")
+
+print(99 % 2)

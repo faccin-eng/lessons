9f4660333c854872cd3a34c5c2b1f40f53b17f33
Faccin-Eng <mjfaccin@hotmail.com>
2025-07-07 17:36:31 -0400

    Projeto Site org

--- turtie.py
+++ turtie.py
@@ -1,11 +1,15 @@
 import turtle
 
-bob = turtle.Turtle()
+bob = turtle.Turtle() #atribui a função turtle a bob
 
-bob.pencolor("blue")  # Move para frente 100 unidades
-bob.forward(50)  # Move para frente 100 unidades
-bob.right(90)     # Gira 90 graus à direita
-bob.pencolor("green")
-bob.forward(50)
+for _ in range (16):
+    bob.pencolor("blue")  # azul
+    bob.forward(50)  # Move para frente 100 unidades
+    bob.right(90)     # Gira 90 graus à direita
+    bob.pencolor("green") #verde
+    bob.forward(50) 
+    bob.left(30) #gira 30 graus para ESQUERDA
+    bob.pencolor("red") #vermelho
+    bob.forward(100)
 
-turtle.mainloop() 
+turtle.mainloop() #mantem a janela aberta

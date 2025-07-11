98b67d90c2fcd9647c80a98019d12994a2d720dd
Faccin-Eng <mjfaccin@hotmail.com>
2025-07-11 15:21:21 -0400

    instrucoes

---
+++ Controles/sorteio.py
@@ -0,0 +1,6 @@
+#random > selecionar um numero de 1 a 999 > atribuir a X
+#input('Digite seu palpite...
+#if palpite = x então CABOU!
+#if palpite < x então dizer que é menor
+#else então dizer que é maior
+

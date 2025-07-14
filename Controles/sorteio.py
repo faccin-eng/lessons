7aece2876a91ed67b7cdde9a32e6dd59e5cc88b0
Jp <bbbhgggfghhf@gmail.com>
2025-07-14 14:05:53 -0400

    Controle

--- Controles/sorteio.py
+++ Controles/sorteio.py
@@ -4,3 +4,26 @@
 #if palpite < x então dizer que é menor
 #else então dizer que é maior
 
+import random
+
+
+repeticao = 0
+palpite = 0
+x = random.randint(1,999)
+while palpite != x:
+    palpite = int(input("Digite um palpite: "))
+    repeticao += 1
+    dif = palpite - x
+        #y = input("Qualquer coisa ")
+    #if palpite == int(y):print("Igual")
+    if palpite == x:
+        print("Você acertou o número {}".format(x))
+        print("Você teve {} repetições".format(repeticao))
+    elif palpite < x and dif < -100:
+        print("O palpite é menor Tá frio")
+    elif palpite < x:
+        print("O palpite é menor")
+    elif palpite > x and dif > 100:
+        print("O palpite é maior Tá frio")
+    else:
+        print("O palpite é maior")

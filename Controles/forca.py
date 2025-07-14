bcb2d20155f0e96389681ec34c68eee32b8972d0
edufaccin <95235903+edufaccin@users.noreply.github.com>
2025-07-14 15:53:55 -0300

    forca

---
+++ Controles/forca.py
@@ -0,0 +1,19 @@
+#Lista de palavras:
+import random
+palavras = [
+    "abacaxi",  # Fruta tropical
+    "banana",   # Fruta amarela 
+    "cachorro", # Animal de estimação
+    "dicionário", # Livro de palavras e seus significados       
+    "elefante", # Animal grande com tromba
+    "futebol",  # Esporte popular
+    "guitarra", # Instrumento musical de cordas
+    "hipopótamo", # Animal grande que vive na água
+    "jacaré",   # Réptil grande que vive em água doce   
+    "kiwi",     # Fruta pequena e verde
+    "leão",     # Animal selvagem conhecido como rei da selva
+]
+
+#Função para escolher uma palavra aleatória 
+palavra_escolhida = random.choice(palavras)
+

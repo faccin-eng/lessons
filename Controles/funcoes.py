664f9c6e5c92e51d8b2fd742af8b741067a54a5f
Faccin-Eng <mjfaccin@hotmail.com>
2025-07-19 18:23:30 -0400

    Exercicio funções

---
+++ Controles/funcoes.py
@@ -0,0 +1,27 @@
+print(type(True))
+#fazer um programa que diga o tipo da variavel digitada
+#x = input(var)
+#fazer uma conversão desse input
+#if x int(x) elif float(x) elif bool(x)
+#print(type(x))
+
+
+
+
+
+# import math
+
+
+# def mathpotencia(a, b):
+#     return float(a ** b)
+
+
+# print(mathpotencia(2,2))
+
+
+
+def print_lyrics():
+    print('Era um garoto que como eu')
+    print('Amava os beatles e os rolling stones')
+
+print_lyrics()
--- cpp/teste.cpp
+++
@@ -1,5 +0,0 @@
-
-int main(int arg, char** argv){
-    std::count <<"Olá, mundo!";
-    return 0;
-}

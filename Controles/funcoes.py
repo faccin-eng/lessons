3bbfaf0bb05a01fbf3d7cbc45afb9fc7b6c9615e
Jp <bbbhgggfghhf@gmail.com>
2025-07-21 13:45:59 -0400

    Atualização Funcões

--- Controles/funcoes.py
+++ Controles/funcoes.py
@@ -1,4 +1,26 @@
-print(type(True))
+import sys
+sair = "n"
+while sair == "n":
+    entrada = input("Digite algo: ")
+    try:
+        variavel = eval(entrada)
+    
+    except:
+        variavel = entrada
+        print(type(entrada))
+    print("Valor digitado:", variavel)
+    print("Tipo da variável:", type(variavel))
+
+    sair = input("Digite 'n' para continuar ou outra tecla para sair: ")
+
+
+
+
+
+
+
+
+
 #fazer um programa que diga o tipo da variavel digitada
 #x = input(var)
 #fazer uma conversão desse input
@@ -17,7 +39,6 @@ print(type(True))
 
 
 # print(mathpotencia(2,2))
-
 
 
 def print_lyrics():
